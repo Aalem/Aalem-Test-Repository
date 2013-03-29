5b2be048476afde82e37200db04ca6c039fbcc73
Aalem <Aalem.daneshyar87@gmail.com>
2013-03-29 10:23:17 +0430

    first commit for test

---
+++ Identity.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Identity{
+
+	public static void main(String[] args){
+		System.out.println("Hello to Github");
+		System.out.println("Enter your Name:");
+		Scanner input = new Scanner(System.in);
+		String name = input.next();
+		System.out.println(name);
+		System.out.println("New Update by Aalem");
+		System.out.println("New New Update by Aalem");
+	}
+}
